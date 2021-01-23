823a52909b604ebc5b54061750cf74eb87684ef0
Earl Tankard, Jr <theflashtank21@gmail.com>
2021-01-22 19:34:25 -0500

    Basic Initial Tip Calculator

---
+++ Prework/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Earl Tankard, Jr. on 1/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        //get the bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        //get total tip
+        let tipPercentages = [0.15, 0.18, 0.20]
+        let tip            = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        //update fields && labels
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text     = String(format: "$%.2f", total)
+        
+        
+        
+    }
+    
+}
+
